5edd113f3a56c8087595051dbe1475eceb0fc563
Dhara <dhara.patel912005@gmail.com>
2025-03-09 18:14:29 +0530

    Add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import { Link,useNavigate } from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import { signInStart,signInFailure,signInSuccess } from '../Redux/user/userSlice'
 
 const Signin = () => {
   const [formData, setformData] = useState({})
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(false)
+  // const [error, setError] = useState(null)
+  // const [loading, setLoading] = useState(false)
+  const {loading,error} = useSelector((state)=>state.user)
   const navigte = useNavigate();
+  const dispatch=useDispatch()
   const handleOnchange = (e) => {
     setformData({
       ...formData,
@@ -18,7 +22,7 @@ const Signin = () => {
   const handleOnsubmit = async (e) => {
     e.preventDefault();
     try{
-      setLoading(true)
+      dispatch(signInStart)
       const res = await fetch('/api/auth/signin',
         {
           method: 'POST',
@@ -30,15 +34,13 @@ const Signin = () => {
       );
       const data = await res.json();
       if (data.success == false) {
-        setLoading(false)
-        setError(data.message)
+        dispatch(signInFailure(data.message));
         return
       }
-      setLoading(false)
-      setError(null)
+      dispatch(signInSuccess(data))
       navigte('/')
     }catch(error){
-  setLoading(false)
+ dispatch(signInFailure(error.message))
   setError(error.message)
     }
     
@@ -54,7 +56,7 @@ const Signin = () => {
         <input type='text' placeholder='password' className='border bg-slate-100 p-3 rounded-lg' id='password' onChange={handleOnchange} />
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
           {
-            loading?'Loading...' : 'Sign Up'
+            loading?'Loading...' : 'Sign in'
           }
         </button>
       </form>

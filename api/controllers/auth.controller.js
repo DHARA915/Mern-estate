3102360e9c0c9c35cece66cd28531cbff8369c9d
Dhara <dhara.patel912005@gmail.com>
2025-03-09 12:13:26 +0530

    complete sign in pafe functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -28,7 +28,7 @@ export const signin = async (req,res,next)=>{
         return next(errorHandler(404,'User Not Found!'))
       }
       const validPassword = bcryptjs.compareSync(password,validUser.password)
-      if(!validPassword) return next(errorHandler(401,'Wrong credential!'))
+      if(!validPassword) return next(errorHandler(401,'Wrong credentials!'))
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
 
       // Remove password from user object before sending response
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
 
 const Signin = () => {
+  const [formData, setformData] = useState({})
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const navigte = useNavigate();
+  const handleOnchange = (e) => {
+    setformData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    })
+  }
+  console.log(formData)
+
+
+  const handleOnsubmit = async (e) => {
+    e.preventDefault();
+    try{
+      setLoading(true)
+      const res = await fetch('/api/auth/signin',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const data = await res.json();
+      if (data.success == false) {
+        setLoading(false)
+        setError(data.message)
+        return
+      }
+      setLoading(false)
+      setError(null)
+      navigte('/')
+    }catch(error){
+  setLoading(false)
+  setError(error.message)
+    }
+    
+  };
+
   return (
-    <div>Signin</div>
+    <div className='p-3 mx-auto max-w-lg'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign-in</h1>
+
+      <form onSubmit={handleOnsubmit} action="" className='flex flex-col gap-4'>
+       
+        <input type='text' placeholder='email' className='border bg-slate-100 p-3 rounded-lg' id='email' onChange={handleOnchange} />
+        <input type='text' placeholder='password' className='border bg-slate-100 p-3 rounded-lg' id='password' onChange={handleOnchange} />
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {
+            loading?'Loading...' : 'Sign Up'
+          }
+        </button>
+      </form>
+
+      <div className='flex gap-2 mt-5'>
+        <p>Do not have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className='text-blue-700'>Sign-up</span>
+        </Link>
+      </div>
+      {
+        error && <p className='text-red-700 mt-5'>
+          {error}
+        </p>
+      }
+    </div>
   )
 }
 

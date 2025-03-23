54d95c03da13123bdf4ed314e004bd0a0db90a01
Dhara <dhara.patel912005@gmail.com>
2025-03-23 11:15:11 +0530

    complete listing page

--- client/src/pages/Listingpage.jsx
+++ client/src/pages/Listingpage.jsx
@@ -4,6 +4,15 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle'
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from 'react-icons/fa';
 
 
 const Listingpage = () => {
@@ -13,6 +22,7 @@ const Listingpage = () => {
     const [listing, setListing] = useState(null)
     const [loading, Setloading] = useState(false)
     const [error, SetError] = useState(false)
+    const [copied, setCopied] = useState(false);
     const params = useParams()
     useEffect(() => {
         const fetchListing = async () => {
@@ -58,12 +68,87 @@ const Listingpage = () => {
         {listing.imageUrls.map((url) => (
           <SwiperSlide key={url}>
             <div
-              className="w-full h-[550px] bg-center bg-cover"
+              className="w-full h-[450px] bg-center bg-cover"
               style={{ backgroundImage: `url(${url})` }}
             ></div>
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className='fixed right-9 top-[12%] z-10 border rounded-full w-10 h-10 flex justify-center items-center bg-slate-100 cursor-pointer'>
+      <FaShare className='text-slate-500' 
+      onClick={()=>{
+        navigator.clipboard.writeText(window.location.href);
+        setCopied(true);
+        setTimeout(()=>{
+          setCopied(false);
+        },2000)
+      }}/>
+      </div>
+       {copied && (
+            <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+              Link copied!
+            </p>
+          )}
+
+        <div className='flex flex-col max-w-4xl mx-auto p-3  gap-4'>
+        <p className='text-2xl font-semibold'>
+          {listing.name} - ${' '}
+          {listing.offer
+                ? listing.discountPrice.toLocaleString('en-US')
+                : listing.regularPrice.toLocaleString('en-US')}
+              {listing.type === 'rent' && ' / month'}
+        </p>
+       <p className='flex items center  gap-2 text-slate-600 my-2 text-sm'>
+      <FaMapMarkerAlt 
+      className='text-green-700'
+      />
+      {listing.address}
+       </p>
+       <div className='flex gap-4'>
+        <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+          {listing.type==='rent' ?'For Rent':'For Sale'}
+        </p>
+        {
+          listing.offer &&(
+            <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+              ${+listing.regularPrice - +listing.discountPrice}
+            </p>
+          )
+        }
+        
+       </div>
+       <p className='text-slate-800'> <span className='font-semibold text-black'>Description -  {' '}</span> 
+          {listing.description}
+        </p>
+        <ul className='flex flex-wrap gap-2 sm:gap-6 items-center text-green-900 font-semibold text-sm'>
+          <li className='flex gap-2 items-center whitespace-nowrap '>
+            <FaBed className='text-lg  '/> 
+            {listing.bedrooms>1 ? `${listing.bedrooms} beds`:`${listing.bedrooms} bed`}
+          </li>
+          <li className='flex gap-2  whitespace-nowrap '>
+            <FaBath className='text-lg  '/> 
+            {listing.bathrooms>1 ? `${listing.bathrooms} baths`:`${listing.bedrooms} bath`}
+          </li>
+
+          <li className='flex gap-2  whitespace-nowrap '>
+            <FaParking className='text-lg  '/> 
+           {
+            listing.parking ?'Parking Spot':'No Parking'
+           }
+          </li>
+
+          <li className='flex gap-2  whitespace-nowrap '>
+            <FaChair className='text-lg  '/> 
+           {
+            listing.furnished ?'Furnished':'Unfurnished'
+           }
+          </li>
+        </ul>
+        </div>
+
+        
+       
+
     </>
   )
 }

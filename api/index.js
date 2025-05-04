a60d66c1dcec40edbee492ece5ebfaf9b9baba5a
Dhara <dhara.patel912005@gmail.com>
2025-05-04 11:32:26 +0530

    remove priviously added array in origin

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://eternahomes.onrender.com"],
+    origin: "http://localhost:5173",
     credentials: true,
     
 }));

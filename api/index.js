081112e8d0051608b08f76ae3024048f3901af02
Dhara <dhara.patel912005@gmail.com>
2025-05-04 11:25:02 +0530

    remove previous added cors

--- api/index.js
+++ api/index.js
@@ -18,18 +18,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-    
-// }));
-
-
 app.use(cors({
-  origin: ["http://localhost:5173", "https://eternahomes.onrender.com"], // add all allowed origins
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true // if you use cookies or sessions
+    origin: "http://localhost:5173",
+    credentials: true,
+    
 }));
+
+
+// app.use(cors({
+//   origin: ["http://localhost:5173", "https://eternahomes.onrender.com"], // add all allowed origins
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true // if you use cookies or sessions
+// }));
 
 
 

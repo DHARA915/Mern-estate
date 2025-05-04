f710f30678f242e6d6f325f55077dac156b3183b
Dhara <dhara.patel912005@gmail.com>
2025-05-04 11:17:06 +0530

    add deployement link in cors

--- api/index.js
+++ api/index.js
@@ -18,11 +18,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
     
+// }));
+const cors = require("cors");
+
+app.use(cors({
+  origin: ["http://localhost:5173", "https://eternahomes.onrender.com"], // add all allowed origins
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true // if you use cookies or sessions
 }));
+
 
 
 
